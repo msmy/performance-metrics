e6a2f3eca2216e8f29cc0a259ebd88592df3c68c
Malcolm Smy <msmy@users.noreply.github.com>
2017-03-07 09:58:19 +0000

    Change path to push gateway (#2)

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 DATA=`cat`
+BRANCH=`git symbolic-ref --short -q HEAD | tr - _`
+JOB=`echo ${PWD##*/} | tr - _`
 HOST=${HOST:-'http://localhost:9091'}
-URL_PATH='/metrics/jobs/perf_metrics/instances/metrics'
+URL_PATH="/metrics/jobs/perf_metrics_${JOB}_${BRANCH}/instances/metrics"
 URL="$HOST$URL_PATH"
 
+echo $URL
+exit 0
 echo "$DATA" | curl --data-binary @- "$URL"
