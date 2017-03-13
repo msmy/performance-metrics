56f5c1fcd4cb35f04e7d41882df41e1f3c1c8467
Malcolm Smy <msmy@sky.com>
2017-03-13 22:58:17 +0000

    stub branch

--- src/utils/formatter/formatter.spec.js
+++ src/utils/formatter/formatter.spec.js
@@ -5,21 +5,20 @@ const gitRevSync = require('git-rev-sync');
 const formatter = require('./formatter');
 
 describe('formatter', () => {
+  const sandbox = sinon.sandbox.create();
+
+  const branch = 'some-branch';
+  const app = 'performance-metrics';
+
+  beforeEach(() => {
+    sandbox.stub(gitRevSync, 'branch').returns(branch);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   describe('addLabels', () => {
-
-    const sandbox = sinon.sandbox.create();
-
-    const branch = 'some-branch';
-    const app = 'performance-metrics';
-
-    beforeEach(() => {
-      sandbox.stub(gitRevSync, 'branch').returns(branch);
-    });
-
-    afterEach(() => {
-      sandbox.restore();
-    });
-
     it('should return custom and default labels', () => {
       const customLabels = {"type": "css", "another": "label"};
 
@@ -42,7 +41,7 @@ describe('formatter', () => {
         my: 'cool',
         amazing: 'labels'
       };
-      const expectedResult = 'perf_metrics_some-module{my="cool",amazing="labels",branch="add-gzip-metric-to-filesize",app="performance-metrics",team="unknown",domain="nowtv"} some-res';
+      const expectedResult = 'perf_metrics_some-module{my="cool",amazing="labels",branch="some-branch",app="performance-metrics",team="unknown",domain="nowtv"} some-res';
       expect(formatter.toPromethesus(module, res, customLabels)).to.equal(expectedResult);
     });
   });
